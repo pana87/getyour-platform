816bfdaa073c68f4f629031c72f3eba185983837
agile-rebell <oderdavid73734@gmail.com>
2024-09-05 12:54:25 +0200

    Update git-initial-commit.sh

--- git-initial-commit.sh
+++ git-initial-commit.sh
@@ -8,13 +8,24 @@ if [ -z "$1" ]; then
 fi
 
 # Parameter zuweisen
-BRANCH_NAME=$1
+BRANCH_NAME=$1 
 
 # Befehle ausführen
 npm run close
+
+#Überprüfung, ob bereits Dateien vorhanden sind
+if [ $(git ls-flies | wc -1) -eq 0]; then
+  echo "Erstelle eine initiale README.md"
+  echo "#PROJEKT $BRANCH_NAME" > README.md
+  git add README.md
+fi 
+
+# Befehle ausführen
 git checkout -b "$BRANCH_NAME"
 git add .
 git commit -m "$BRANCH_NAME"
 git push --set-upstream origin "$BRANCH_NAME"
+git tag -a "v1.0.0" -m 
+git push origin "v1.0.0"
 git branch
 git status
