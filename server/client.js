13a9ba467c9551755acc237070d1014ea54a96a9
pana87 <p.tsivelekidis@gmail.com>
2024-11-12 15:53:57 +0100

    home-issue-on-firefox

--- server/client.js
+++ server/client.js
@@ -80,7 +80,7 @@ app.use(express.json({limit: "50mb"}))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(removeCookies)
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "script-src 'self';");
+  res.setHeader("Content-Security-Policy", "script-src 'self' https://www.get-your.de;");
   res.setHeader('X-Frame-Options', 'DENY')
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
   res.setHeader('Pragma', 'no-cache')
